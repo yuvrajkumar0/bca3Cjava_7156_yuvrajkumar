78b5794c1ca79ca902438783897d4ccfb7ae4543
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-07-11 14:44:44 +0530

    Add files via upload

---
+++ program_4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class program_4{
+    public static void main(String args []) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your firstNumber: ");
+        int a = input.nextInt();
+
+	System.out.print("Enter your secondNumber: ");
+        int b = input.nextInt();
+
+	 System.out.println(a+b);
+ 	 System.out.println(a-b);
+	 System.out.println(a*b);
+         System.out.println(a/b);
+	
+     
+	}
+}
