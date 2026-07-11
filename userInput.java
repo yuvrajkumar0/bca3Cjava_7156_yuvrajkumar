b284061881661a88e49b5f9523bd6707987e3a00
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-07-11 14:43:21 +0530

    Add files via upload

---
+++ userInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class userInput{
+    public static void main(String args []) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+        System.out.print(name);
+
+     
+     }
+}
