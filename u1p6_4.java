9762711bc597d42d32985e913e89e4b37f4f2960
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-07-11 15:26:51 +0530

    Add files via upload

---
+++ u1p6_4.java
@@ -0,0 +1,18 @@
+	import java.util.Scanner;
+	public class u1p6_4{
+	public static void main(String args[])
+
+	{
+
+	     Scanner input = new Scanner(System.in); 
+             System.out.print("enter your name: ");
+	     String name = input.nextLine();
+         
+	     for(int i=0; i<10; i++)
+
+		{
+		    System.out.println(name);
+                }
+	}
+
+	}
