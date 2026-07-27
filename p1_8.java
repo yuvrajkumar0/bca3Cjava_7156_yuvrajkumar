d3a028c0c254bec4931121a7fdf8adadf68311c2
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-07-27 23:17:13 +0530

    my challenge no.8

---
+++ p1_8.java
@@ -0,0 +1,11 @@
+public class p1_8{
+    public static void main(String args[]){
+        int num =Integer.parseInt(args[0]);
+        if(num%2==0){
+            System.out.print(num + "is even");
+        }
+        else{
+            System.out.println(num+ "is ODD");
+        }
+    }
+}
