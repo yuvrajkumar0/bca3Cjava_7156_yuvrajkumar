da73b3d646d5b3a6479bd721c04195d031765f11
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-09-01 18:40:14 +0530

    pattern serise

---
+++ prog9pattern1.java
@@ -0,0 +1,16 @@
+//write a java program to print the pattern1.
+
+class prog9pattern1{
+
+     public static void main (String args[]){
+
+    
+    for(int rows=1;rows<=5;rows++){
+     
+      for(int cols=1;cols<=rows;cols++){
+          System.out.print("* ");
+      }
+      System.out.println("");
+    }
+     }
+}
