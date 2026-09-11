bc6a3ca465d082797623114321ce8cf77b7dfbe0
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-09-11 15:59:20 +0530

    Add files via upload

---
+++ ad.java
@@ -0,0 +1,13 @@
+package addition;
+public class ad{
+	int sum;
+	public void calad(int a, int b)
+	{
+		sum = a+b;
+	}
+	
+	public void disad()
+	{
+		System.out.println("Addition is: " + sum);
+	}
+}
