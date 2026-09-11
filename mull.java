791f8d0703651ec3cd95513b018003a15d0d938d
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-09-11 16:00:22 +0530

    Add files via upload

---
+++ mull.java
@@ -0,0 +1,13 @@
+package multiplication;
+public class mull{
+	int mult;
+	public void calmull(int a, int b)
+	{
+		mult = a*b;
+	}
+	
+	public void dismull()
+	{
+		System.out.println("multiplication is: " + mult);
+	}
+}
