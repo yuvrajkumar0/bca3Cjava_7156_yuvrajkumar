8af0deeacfd015af6828aa655e76b607fe6b733b
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-09-11 16:00:00 +0530

    Add files via upload

---
+++ divv.java
@@ -0,0 +1,13 @@
+package division;
+public class divv{
+	int divi;
+	public void caldivv(int a, int b)
+	{
+		divi = a/b;
+	}
+	
+	public void disdivv()
+	{
+		System.out.println("division: " + divi);
+	}
+}
