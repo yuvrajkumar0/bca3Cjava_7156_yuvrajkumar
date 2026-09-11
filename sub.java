cba7eadff22a3fd735cd7851d6b96ad1f8e47644
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-09-11 16:00:35 +0530

    Add files via upload

---
+++ sub.java
@@ -0,0 +1,13 @@
+package subtraction;
+public class sub{
+	int subt;
+	public void calsub(int a, int b)
+	{
+		subt = a-b;
+	}
+	
+	public void dissub()
+	{
+		System.out.println("subtraction is: " + subt);
+	}
+}
