f0c376b0fea3ba879164f2f0b11e97ce772da6d0
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-09-16 14:30:41 +0530

    Add files via upload
    exception error genereate code

---
+++ text.java
@@ -0,0 +1,14 @@
+public class text{
+	public static void main(String[] args){
+		String studentName = "yuvraj";
+		System.out.println(studentName + " started math calculation..");
+		
+		 
+			int totalMarks= 100;
+			int subjects=0;
+			int result = totalMarks/subjects;
+			
+			System.out.println("result"+ result);
+		 
+	}
+}
